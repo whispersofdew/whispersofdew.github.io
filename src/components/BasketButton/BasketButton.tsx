8a89f740057854a981d242930c8e76450990383c
alexey.korolev <whispersofdew@gmail.com>
2025-02-11 19:41:14 +0300

    From JS to TypeScript

--- src/components/BasketButton/BasketButton.tsx
+++ src/components/BasketButton/BasketButton.tsx
@@ -2,29 +2,13 @@ import React from 'react';
 import s from './basketbutton.module.sass';
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
-
   count: number;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export function BasketButton({ primary = false, size = 'medium', backgroundColor, ...props }: ButtonProps) {
   const count = props.count;
   const mode = primary ? s.primary : s.secondary;
